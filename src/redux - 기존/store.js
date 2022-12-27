c7f8656344851dc65e8285f7fa7e5e89c6e4dd18
nasmakd <110151531+nasmakd@users.noreply.github.com>
2022-12-27 09:36:54 +0900

    Delete src/redux - 기존 directory

--- src/redux - 기존/store.js
+++
@@ -1,9 +0,0 @@
-import { legacy_createStore as createStore, applyMiddleware } from 'redux';
-// createStore 가로줄, redux toolkit 사용 유도를 위해
-import rootReducer from './reducers';
-import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
-
-let store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
-
-export default store;
